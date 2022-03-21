d44cc9a5322822765a1c59e141135d9454b35445
John Collera <johncollera02@gmail.com>
2022-03-20 23:37:54 -0400

    Challenge commit

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -7,16 +7,15 @@ public class CameraScript : MonoBehaviour
 
     public GameObject target;
 
-    
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        this.transform.position = new Vector3(target.transform.position.x, this.transform.position.y, this.transform.position.z);
+        this.transform.position = new Vector3(target.transform.position.x, target.transform.position.y + 1, this.transform.position.z);
     }
 }
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,30 +6,43 @@ using UnityEngine.UI;
 public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rd2d;
-
     public float speed;
-
     public float jumpForce;
-
-    public Text score;
-
-    private int scoreValue = 0;
-    
-    private bool facingRight = true;
-
-    private bool isOnGround;
-
-    public Transform groundcheck;
-
+    public float jumpForce2;
     public float checkRadius;
-
+    public float checkRadius2;
+    public Text score;
+    public Text lives;
+    public Text endMess;
+    public Text introText;
+    private int scoreValue;
+    private int livesValue;
+    private bool facingRight = true;
+    private bool isOnGround;
+    private bool isOnSide;
+    private bool hasWon = false;
+    private bool canMove;
+    public Transform groundCheck;
+    public Transform sideCheck;
     public LayerMask allGround;
+    public AudioClip music;
+    public AudioClip sound;
+    public AudioSource musicSource;
+    Animator anim;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        anim = GetComponent<Animator>();
+        scoreValue = 0;
+        livesValue = 3;
+        canMove = true;
+        SetCountText();
+        musicSource.clip = music;
+        musicSource.Play();
+        Invoke("DisableText", 10f);
     }
 
     // Update is called once per frame
@@ -37,29 +50,50 @@ public class PlayerScript : MonoBehaviour
     {
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
-        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
-        isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
+        if(canMove == true)
+        { 
+            rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+            isOnGround = Physics2D.OverlapCircle(groundCheck.position, checkRadius, allGround);
+            isOnSide = Physics2D.OverlapCircle(sideCheck.position, checkRadius2, allGround);
 
-        if(facingRight == false && hozMovement > 0)
+        }
+        
+        if(facingRight == false && hozMovement > 0 && canMove)
         {
             Flip();
         }
 
-        else if(facingRight == true && hozMovement < 0)
+        else if(facingRight == true && hozMovement < 0 && canMove)
         {
             Flip();
         }
 
-        if(hozMovement > 0 && facingRight == true)
+        if(rd2d.velocity.magnitude > 0 && isOnGround == true && canMove)
+        {
+            anim.SetInteger("State", 1);
+        }
+        
+        else if(rd2d.velocity.magnitude <= 0 && isOnGround == true && canMove)
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        else if(vertMovement > 0 && isOnGround == false && canMove)
+        {
+            anim.SetInteger("State", 2);
+        }
+
+        if(hozMovement > 0 && isOnGround == true)
         {
             Debug.Log ("Facing Right");
         }
 
-        if(hozMovement < 0 && facingRight == false)
+        if(hozMovement < 0 && isOnGround == true)
         {
             Debug.Log ("Facing Left");
         }
 
+        
         if(vertMovement > 0 && isOnGround == false)
         {
             Debug.Log ("Jumping");
@@ -76,24 +110,71 @@ public class PlayerScript : MonoBehaviour
         }
     }
 
+    void DisableText()
+    { 
+      introText.enabled = false; 
+    }  
+
+    void SetCountText()
+    {
+        score.text = "Score: " + scoreValue.ToString();
+        lives.text = "Lives: " + livesValue.ToString();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.collider.tag == "Coin")
         {
-            scoreValue += 1;
-            score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+            scoreValue++;
+            SetCountText();
+
+            if(scoreValue == 4)
+            {
+                transform.position = new Vector2(200.0f, 0.0f);
+                livesValue = 3;
+                SetCountText();
+            }
         }
 
+        else if(collision.collider.tag == "Enemy")
+        {
+            Destroy(collision.collider.gameObject);
+            livesValue--;
+            SetCountText();
+        }
+
+        if(scoreValue == 8 && hasWon == false)
+        {
+            hasWon = true;
+            endMess.text = "\n\n\n\n\n\n\n\n\nYou Won!\nGame by John Collera";
+            musicSource.clip = sound;
+            musicSource.Play();
+            musicSource.loop = false;
+            canMove = false;
+        }
+
+        if(livesValue <= 0)
+        {
+            endMess.text = "\n\n\n\n\n\n\n\n\nYou Lost!\nGame by John Collera";
+            rd2d.gameObject.SetActive(false);
+        }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if(collision.collider.tag == "Ground" && isOnGround)
+        if(collision.collider.tag == "Ground" && isOnGround && canMove)
         {
             if(Input.GetKey(KeyCode.W))
             {
-                rd2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse); //the 3 in this line of code is the player's "jumpforce," and you change that number to get different jump behaviors.  You can also create a public variable for it and then edit it in the inspector.
+                rd2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            }
+        }
+        if(collision.collider.tag == "Ground" && isOnSide && canMove)
+        {
+            if(Input.GetKey(KeyCode.W))
+            {
+                rd2d.AddForce(new Vector2(0, jumpForce2), ForceMode2D.Impulse); 
             }
         }
     }
@@ -102,6 +183,7 @@ public class PlayerScript : MonoBehaviour
     {
         facingRight = !facingRight;
         Vector2 Scaler = transform.localScale;
-        Scaler.x = Scaler.x * -1
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
     }
 }
